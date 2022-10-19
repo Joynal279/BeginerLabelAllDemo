8afd84f3fdf16aff1b585e716052baaa111edf66
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2022-10-19 22:25:22 +0600

    Initial Commit

---
+++ BeginerLabelAllDemo/BeginerLabelAllDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeginerLabelAllDemoApp.swift
+//  BeginerLabelAllDemo
+//
+//  Created by JOYNAL ABEDIN on 19/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BeginerLabelAllDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
